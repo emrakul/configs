68a87732654213385a64f10d955778f8f02df8a6
Nikita Savelyev <nasavelev@beeline.ru>
2022-05-23 19:14:11 +0300

    my configs for laptop asus g14

---
+++ polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q i3bar
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar main &
+
+echo "Polybar launched..."
